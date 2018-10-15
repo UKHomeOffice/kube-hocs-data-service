55761f508367821f4a3b93ab3dcbe29a67e650d8
Nick McMahon <nick.mcmahon@digital.homeoffice.gov.uk>
2018-10-15 11:15:39 +0100

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,39 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-if [ $ENVIRONMENT == "prod" ]
-then
-    export KUBE_TOKEN=${PROD_KUBE_TOKEN}
+export KUBE_NAMESPACE=${KUBE_NAMESPACE}
+export KUBE_SERVER=${KUBE_SERVER}
+
+if [[ -z ${VERSION} ]] ; then
+    export VERSION=${IMAGE_VERSION}
+fi
+
+if [[ ${ENVIRONMENT} == "prod" ]] ; then
+    echo "deploy ${VERSION} to prod namespace, using HOCS_DATA_SERVICE_PROD drone secret"
+    export KUBE_TOKEN=${HOCS_DATA_SERVICE}
+    export REPLICAS="2"
     export DNS_PREFIX=
 else
     export DNS_PREFIX=${ENVIRONMENT}.notprod.
+    if [[ ${ENVIRONMENT} == "qa" ]] ; then
+        echo "deploy ${VERSION} to test namespace, using HOCS_DATA_SERVICE_QA drone secret"
+        export KUBE_TOKEN=${HOCS_DATA_SERVICE_QA}
+        export REPLICAS="2"
+    else
+        echo "deploy ${VERSION} to dev namespace, using HOCS_DATA_SERVICE_DEV drone secret"
+        export KUBE_TOKEN=${HOCS_DATA_SERVICE_DEV}
+        export REPLICAS="1"
+    fi
+fi
+
+if [[ -z ${KUBE_TOKEN} ]] ; then
+    echo "Failed to find a value for KUBE_TOKEN - exiting"
+    exit -1
 fi
 
 cd kd
-kd --insecure-skip-tls-verify --timeout 5m0s \
-   --file ingress.yaml \
-   --file service.yaml \
-   --file deployment.yaml
+
+kd --insecure-skip-tls-verify \
+   --timeout 10m \
+   -f ingress.yaml \
+   -f service.yaml \
+   -f deployment.yaml
